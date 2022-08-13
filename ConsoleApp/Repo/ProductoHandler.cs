b3323515c72f9a002e0777113cff8ba136f250e4
Ale-p650 <ale.pavlinusich@gmail.com>
2022-08-13 16:25:15 -0300

    Primer Handler Implementado
    Se implementó "ProductoHandler", establece una conexion con la BD SistemaGestion y la Tabla Producto, hace una lectura con DataReader y hace una instancia de la clase Producto por cada producto y asigna los valores de la tabla a los properties del modelo mismo.

---
+++ ConsoleApp/Repo/ProductoHandler.cs
@@ -0,0 +1,49 @@
+﻿using ConsoleApp.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ConsoleApp
+{
+    public class ProductoHandler : DBHandler
+    {
+    
+        public List<Producto> GetProducto()
+        {
+            List<Producto> products = new List<Producto>();
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                string query = "SELECT * FROM PRODUCTO";
+                
+                using (SqlCommand command = new SqlCommand(query, conn))
+                {
+                    conn.Open();
+                    using (SqlDataReader dr = command.ExecuteReader())
+                    {
+                        if (dr.HasRows)
+                        {
+                            while (dr.Read())
+                            {
+                                Producto producto = new Producto();
+                                producto.Id = Convert.ToInt32(dr["Id"]);
+                                producto.Descripciones = Convert.ToString(dr["Descripciones"]);
+                                producto.Costo = Convert.ToDouble(dr["Costo"]);
+                                producto.PrecioVenta = Convert.ToDouble(dr["PrecioVenta"]);
+                                producto.Stock = Convert.ToInt32(dr["Stock"]);
+                                producto.IdUsuario = Convert.ToInt32(dr["IdUsuario"]);
+
+                                products.Add(producto);
+                            }
+                        }
+                    }
+                    conn.Close();
+                }
+            }
+
+            return products;
+        }
+
+
+    }
+
+}
+
