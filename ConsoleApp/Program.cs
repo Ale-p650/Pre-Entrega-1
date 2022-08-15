b994133e4b3087fb0fb1ffd912f3eb86407e0e76
Ale-p650 <ale.pavlinusich@gmail.com>
2022-08-14 21:53:29 -0300

    Llamado a primer metodo Handler
    Se llama por primera vez a un metodo handler, ProductoHandler especificamente, desde el Program.cs

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿namespace ConsoleApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductoHandler prod = new ProductoHandler();
+            prod.GetProducto();
+        }
+    }
+
+
+
+}
